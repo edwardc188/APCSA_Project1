269301d5c7fc10356cbff0ed7d7896c8a4027ab9
edwardc188 <edwardc188@nycstudents.net>
2026-01-17 15:49:03 -0500

    player comments

--- Player.java
+++ Player.java
@@ -7,7 +7,7 @@ public class Player {
     private boolean inShop = false;
     private boolean inCave = false;
     private boolean win = false;
-    private int shopTier = 0;
+    private int shopTier = 0; // determines what items shop sells
 
     public Player() { }
 
@@ -37,15 +37,15 @@ public class Player {
     }
 
     public void enterCave() {
-        Cave darkCave = new Cave((int) (Math.random() * 4) + 1);
-        ArrayList<Crystal> crystalOptions = darkCave.getCrystals();
-        for (int i = 1; i <= crystalOptions.size(); i++) {
+        Cave darkCave = new Cave((int) (Math.random() * 4) + 1); // creates a cave with random size
+        ArrayList<Crystal> crystalOptions = darkCave.getCrystals(); // stores all crystal objects into an ArrayList 
+        for (int i = 1; i <= crystalOptions.size(); i++) { // prints all crystals with odds and values
             Crystal crystal = crystalOptions.get(i - 1);
             System.out.println("Crystal " + i + ": " + ((1000 - crystal.getOdds()) / 10.0) + "% success chance, Worth " + crystal.getValue() + "$");
         }
         Scanner miner = new Scanner(System.in);
         int selection = 1000;
-        while (selection > crystalOptions.size() || selection <= 0) {
+        while (selection > crystalOptions.size() || selection <= 0) { // prevents invalid input
             try {
                 selection = miner.nextInt();
             } catch (InputMismatchException e) {
@@ -55,7 +55,7 @@ public class Player {
             }
 
         }
-        int mineCondition = crystalOptions.get(selection-1).mine();
+        int mineCondition = crystalOptions.get(selection-1).mine(); // determines if a crystal broke or how much to add to balance
         if (mineCondition == -1) {
             System.out.println("The crystal broke.");
         } else {
@@ -67,6 +67,7 @@ public class Player {
         System.out.println("-------------------------");
         startGame();
     }
+    
     public void enterShop() {
         Scanner shopScan = new Scanner(System.in);
         inShop = true;
@@ -76,7 +77,7 @@ public class Player {
         if (shopOption == 1) {
             while (inShop) {
                 int buyValue = stuff.buy();
-                if (buyValue < 4) {
+                if (buyValue < 4) { // buy() returns 0, 1, 2, or 3 upon a successful purchase depending on the item, 4 upon an exit request and 5 if the item is too expensive
                     System.out.println("You bought your dream item!");
                     win = true;
                     System.exit(0);
@@ -87,7 +88,7 @@ public class Player {
                     System.out.println("You cannot buy that item.");
                 }
             }
-        } else if (shopOption == 2) {
+        } else if (shopOption == 2) { // randomly generates new shop tier
             if (money >= 10000) {
                 shopTier = stuff.switchShop();
                 money -= 10000;
@@ -95,10 +96,9 @@ public class Player {
                 System.out.println("You do not have enough money.");
             }
             enterShop();
-        } else {
+        } else { // goes back to start menu
             inShop = false;
             startGame();
         }
-
     }
 }
